360e9f2a85e903c473e85a766aac17a06141cdc6
Kentaro Tanaka <tanaka.kentaro@donuts.ne.jp>
2018-08-20 09:28:22 +0900

    enumerable-module: case01

---
+++ studies/enumerable-module/case01.rb
@@ -0,0 +1,34 @@
+p [1, 2, 3].inject{|x, y| x + y ** 2} rescue p $!
+p [1, 2, 3].inject(0){|x, y| x + y ** 2} rescue p $!
+p [1, 2, 3].inject([]){|x, y| x << y ** 2} rescue p $!
+p [1, 2, 3].inject do|x, y| x + y ** 2 end rescue p $!
+
+p '---1行目---------------------'
+begin
+  [1, 2, 3].inject {|x, y|
+    print sprintf("%d + %d ** 2 = ", x, y)
+    p x + y ** 2
+  }
+rescue
+  p $!
+end
+
+p '---2行目---------------------'
+begin
+  [1, 2, 3].inject(0) {|x, y|
+    print sprintf("%d + %d ** 2 = ", x, y)
+    p x + y ** 2
+  }
+rescue
+  p $!
+end
+
+p '---3行目---------------------'
+begin
+  [1, 2, 3].inject([]) {|x, y|
+    # print sprintf("%d + %d ** 2 = ", x, y)
+    p x << y ** 2
+  }
+rescue
+  p $!
+end
