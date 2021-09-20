7c64e747a86d606779af4251d6c1ac079b4b16c6
tanaken0515 <tanaka.kentaro.kk1222@gmail.com>
2021-09-20 16:28:41 +0900

    ex26

---
+++ gold/ex26/choice01.rb
@@ -0,0 +1,23 @@
+class IPAddr
+  include Enumerable
+
+  def initialize(ip_addr)
+    @ip_addr = ip_addr
+  end
+
+  def each
+    return to_enum unless block_given?
+
+    @ip_addr.split('.').each do |octet|
+      yield octet
+    end
+  end
+end
+
+addr = IPAddr.new("192.10.20.30")
+enum = addr.each
+
+p enum.next # 192と表示される
+p enum.next # 10と表示される
+p enum.next # 20と表示される
+p enum.next # 30と表示される
---
+++ gold/ex26/sample02.rb
@@ -0,0 +1,21 @@
+class IPAddr
+  def initialize(ip_addr)
+    @ip_addr = ip_addr
+  end
+
+  def each
+    return enum_for unless block_given?
+
+    @ip_addr.split('.').each do |octet|
+      yield octet
+    end
+  end
+end
+
+addr = IPAddr.new("192.10.20.30")
+enum = addr.each
+
+p enum.next # 192と表示される
+p enum.next # 10と表示される
+p enum.next # 20と表示される
+p enum.next # 30と表示される
