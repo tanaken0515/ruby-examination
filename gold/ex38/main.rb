aefb23aef5192660ef124dba7f4189fbf295eb0f
tanaken0515 <tanaka.kentaro.kk1222@gmail.com>
2021-10-04 20:53:17 +0900

    ex38

---
+++ gold/ex38/main.rb
@@ -0,0 +1,19 @@
+class C
+  CONST = "Good, night"
+end
+
+module M
+  CONST = "Good, evening"
+end
+
+module M
+  class C
+    CONST = "Hello, world"
+  end
+end
+
+module M
+  class ::C
+    p CONST
+  end
+end
